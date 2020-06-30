99d38a071cf3b2215bb1d9d26f337cca473dc351
Louis Liu <zliu011@beckman.com>
2020-06-30 16:49:35 +0800

    [update] add fcs_file controller
    generate fcs_file related file

--- app/controllers/fcs_files_controller.rb
+++ app/controllers/fcs_files_controller.rb
@@ -1,13 +1,31 @@
 class FcsFilesController < ApplicationController
   def index
+    @fcs_files = FcsFile.all
   end
 
   def new
+    @fcs_file = FcsFile.new
   end
 
   def create
+    @fcs_file = FcsFile.new(fcs_params)
+
+    if @fcs_file.after_save
+      redirect_to fcs_file_path, notice: "uploaed"
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @fcs_file = FcsFile.find(params[:id])
+    @fcs_file.destory
+
+    redirect_to fcs_file_path, notice: "deleted"
   end
+
+  private
+    def fcs_params
+      params.require(:fcs_file).permit(:name, :attachment)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'fcs_files/index'
-  get 'fcs_files/new'
-  get 'fcs_files/create'
-  get 'fcs_files/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'welcome/index'
-  post 'welcome/upload'
+  resources :fcs_files, only: [:index, :new, :create, :destroy]
 end
