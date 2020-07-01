a2e71284096e0375de3223a0d7f78ee7fc95d405
Louis Liu <zliu011@beckman.com>
2020-07-01 16:41:12 +0800

    [update] add open file logic
    add some logic to open file and use fcs parser to read fcs file
    
    currently fcs file can't be readed since the fcs parser is not ready

--- app/controllers/fcs_files_controller.rb
+++ app/controllers/fcs_files_controller.rb
@@ -1,3 +1,5 @@
+require 'fcsparse'
+
 class FcsFilesController < ApplicationController
   def index
     @fcs_files = FcsFile.all
@@ -12,24 +14,35 @@ class FcsFilesController < ApplicationController
 
     saved_file = @fcs_file.save
     if saved_file
-      redirect_to fcs_files_path, notice: "uploaed"
+      redirect_to fcs_files_path, notice: 'uploaed'
     else
-      render "new"
+      render 'new'
     end
   end
 
   def show
     @fcs_file = FcsFile.find(params[:id])
+    file_path = "#{Dir.pwd}/public#{@fcs_file.attachment_url}"
+    begin
+      file = File.open(file_path)
+      file_data = file.read
+      puts 'file read done'
+      FCSParse.process_file(file_path)
+      puts 'fcs file parsed!'
+    rescue StandardError => e
+      redirect_to fcs_files_path, notice: e.message
+    end
   end
 
   def destroy
     @fcs_file = FcsFile.find(params[:id])
     @fcs_file.destroy
-    redirect_to fcs_files_path, notice: "deleted"
+    redirect_to fcs_files_path, notice: 'deleted'
   end
 
   private
-    def fcs_params
-      params.require(:fcs_file).permit(:name, :attachment)
-    end
+
+  def fcs_params
+    params.require(:fcs_file).permit(:name, :attachment)
+  end
 end
