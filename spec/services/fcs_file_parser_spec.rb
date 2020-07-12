29831749faf26fdb8acea8ba27ff2078c9aec0ff
Louis Liu <zliu011@beckman.com>
2020-07-12 10:07:07 +0800

    [update] update test code
    follow 3a pattern

--- spec/services/fcs_file_parser_spec.rb
+++ spec/services/fcs_file_parser_spec.rb
@@ -1,23 +1,29 @@
+# frozen_string_literal: true
+
 require('./app/services/fcs_file_parser')
 
-describe FcsFileParser do
+RSpec.describe FcsFileParser do
   it 'will cal FCSParse to parse the fcs file' do
+    # arrange
     expect(FCSParse).to  receive(:process_file).with('/parser/open_this_file.fcs')
-
+    # act
     parser = FcsFileParser.new
     parser.open('/parser/open_this_file.fcs')
   end
 
   context 'when parse succfully' do
     before do
+      # arrange
       allow(FCSParse).to receive(:process_file) do
         'this is a test fcs file'
       end
     end
 
     it 'return the fcs file content' do
+      # act
       parser = FcsFileParser.new
       file_content = parser.open '/parser/open_this_file.fcs'
+      # assert
       expect(parser.name).to eq('open_this_file.fcs')
       expect(file_content).to eq('this is a test fcs file')
     end
@@ -25,18 +31,18 @@ describe FcsFileParser do
 
   context 'when parse failed' do
     before do
-        allow(FCSParse).to receive(:process_file).and_raise("Unexpect error")
+      # arrange
+      allow(FCSParse).to receive(:process_file).and_raise('Unexpect error')
     end
 
     it 'throw the error message when failed to parse' do
-      
+      # act
       parser = FcsFileParser.new
-      begin
-        file_content = parser.open '/parser/open_this_file.fcs'
-        expect(0).to eq(1) 
-      rescue StandardError => e
-        expect(e.message).to eq('Unexpect error')
-      end
+      parser.open '/parser/open_this_file.fcs'
+      expect(0).to eq(1)
+    rescue StandardError => e
+      # assert
+      expect(e.message).to eq('Unexpect error')
     end
   end
 end
--- spec/utils/project_config_spec.rb
+++ spec/utils/project_config_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe ProjectConfig do
   context 'read config successfully' do
     before do
+      # arrange
       allow(Rails.configuration).to receive(:project_config) do
         {
           :aws => {
@@ -16,16 +17,19 @@ RSpec.describe ProjectConfig do
     end
 
     it 'return aws s3 bucket name from config file' do
+      # assert
       expect(ProjectConfig.aws_s3_bucket_name).to eq('test_bucket_name')
     end
 
     it 'return asw s3 region from config file' do
+      # assert
       expect(ProjectConfig.aws_s3_region).to eq('test_region')
     end
   end
 
-  context 'failed to read config' do
+  context 'failed to read config' do    
     before do
+      # arrange
       allow(Rails.configuration).to receive(:project_config) do
         {
           :aws => {}
@@ -35,14 +39,17 @@ RSpec.describe ProjectConfig do
 
     it 'raise error since aws s3 bucket name not exist' do
       begin
+        # act
         ProjectConfig.aws_s3_bucket_name
         expect(1).to eq(0)
       rescue StandardError => e
+        # assert
         expect(e.message).to eq('can not find s3_bucket_name from aws')
       end
     end
 
     it 'return no_region as default value' do
+      # assert
       expect(ProjectConfig.aws_s3_region).to eq('no_region')
     end
   end
